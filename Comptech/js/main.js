5a5b9294936b13cd66e7f0ce8fc5471a9fb687a1
tonderai chinya <tonderaiclemzc5@gmail.com>
2020-05-26 03:20:35 +0200

    added js

---
+++ Comptech/js/main.js
@@ -0,0 +1,15 @@
+const phoneNumber = document.getElementById("phoneNumber");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
+
+form.addEventListener("submit", (e) => {
+  let messages = [];
+  if (phoneNumber.value == "") {
+    messages.push("Please enter a number!");
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerText = messages.join(", ");
+  }
+});
